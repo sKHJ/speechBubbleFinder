99402eeadba3000e76cda347561e7f0e1fe40721
KHJ <gkswnswnwkr@gmail.com>
2018-06-13 22:16:56 +0900

    Update bubbleHelper.py

--- PycharmProjects/untitled/bubbleHelper.py
+++ PycharmProjects/untitled/bubbleHelper.py
@@ -4,9 +4,13 @@ import numpy as np
 
 drawing = False #Mouse가 클릭된 상태 확인용
 ix,iy = -1,-1
-index=0
+index=48
 
+imageFlag=0
 data=[]
+row = 0
+col = 0
+
 # Mouse Callback함수
 # NOTICE! 항상 좌상단에서 우하단으로 그릴것!
 def draw_rect(event, x,y, flags, param):
@@ -21,11 +25,25 @@ def draw_rect(event, x,y, flags, param):
             cv2.rectangle(img,(ix,iy),(x,y),(255,0,0),-1)
 
     elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False;             # 마우스를 때면 상태 변경
+        drawing = False            # 마우스를 때면 상태 변경
         cv2.rectangle(img,(ix,iy),(x,y),(255,0,0),-1)
-        print(ix,iy,x-ix,y-iy)   # 중심좌표, 너비, 높이 형태로 표현
 
-        data.append([ix,iy,x-ix,y-iy])
+        if imageFlag == 1 :     # 1/3
+            print(ix,iy,x-ix,y-iy)   # 중심좌표, 너비, 높이 형태로 표현
+            data.append([ix,iy,x-ix,y-iy])
+        if imageFlag == 2 :     # 2/3
+            a = x-ix
+            b = y-iy
+            iy = iy + (int)(col/6)
+            print(ix, iy, a, b)
+            data.append([ix, iy, a, b])
+        if imageFlag == 0 :     # 3
+            a = x - ix
+            b = y - iy
+            iy = iy + (int)(col/3)
+            print(ix, iy, a, b)
+            data.append([ix, iy, a, b])
+
 
 
 def num_generator(current_num):
@@ -64,17 +82,36 @@ if __name__ == '__main__':
 
     while True:
         #-------------------------------------
-        jfname = num_generator(index) + ".jpg"
-        csvname = num_generator(index) + ".csv"
-        img = cv2.imread(jfname)
-        if img is None :
+        if imageFlag == 0:
+            jfname = num_generator(index) + ".jpg"
+            csvname = num_generator(index) + ".csv"
+            oriimg = cv2.imread(jfname)
+            print("file open : %s"%jfname)
+
+
+            f = open(csvname, 'w', encoding='utf-8', newline='')
+            wr = csv.writer(f)
+
+        if oriimg is None :
             print ("That was last image")
             break
         else:
-            img = cv2.imread(jfname) / 255
+            row = oriimg.shape[1]
+            col = oriimg.shape[0]
+            #img = cv2.imread(jfname) / 255
 
-        f = open(csvname, 'w', encoding='utf-8', newline='')
-        wr = csv.writer(f)
+            if imageFlag == 0 :
+                img = oriimg[0:(int)((col*2)/3), 0:row] / 255
+                imageFlag+=1
+            elif imageFlag == 1 :
+                img = oriimg[(int)(col/6) : (int)((col*5)/6), 0:col] / 255
+                imageFlag+=1
+            elif imageFlag == 2 :
+                img = oriimg[(int)(col/3):col, 0:col] / 255
+                imageFlag=0
+
+
+
 
         not_mask = np.ones(img.shape, np.float32)
 
@@ -90,13 +127,18 @@ if __name__ == '__main__':
             cv2.imshow(num_generator(index), screen)
             k = cv2.waitKey(1) & 0xFF
             if k == 13:  # ESC = 27, CarriageReturn(enter) = 13
-                cv2.destroyWindow(num_generator(index))
-                wr.writerow(data)
-                data=[]
-                index = index + 1
-                print(num_generator(index))
-                break
+                if imageFlag != 0:
+                    print(1)
+                    break
+                else:
+                    cv2.destroyWindow(num_generator(index))
+                    wr.writerow(data)
+                    data=[]
+                    index = index + 1
+                    print(num_generator(index))
+                    break
 
-        wr.writerow(data) # for last data
-        f.close()
+        if imageFlag == 0:
+            wr.writerow(data) # for last data
+            f.close()
 
