b956386240e89ff6c5578c0741b68582d48e9165
KHJ <gkswnswnwkr@gmail.com>
2018-08-12 23:18:41 +0900

    Update image_classifier.py

--- PycharmProjects/untitled/image_classifier.py
+++ PycharmProjects/untitled/image_classifier.py
@@ -2,9 +2,36 @@ import cv2
 import sys
 import csv
 
-#imageLocation = sys.argv[1]
-imageLocation ='seg_answers'
+# HOW TO USE--------------------------
+# python image_classifier.py [imagefolder]
+# EX ) python image_classifier.py answers
+#-----------------------------------------
+
+
+imageLocation = sys.argv[1]
+#imageLocation ='seg_answers'
 import os
+
+def stateprinter(f1,f2,f3):
+    lang ='JPN'
+    if f1==0:
+        lang='JPN'
+    if f1==1:
+        lang='ENG'
+
+    margin='YES'
+    if f2==0:
+        margin='YES'
+    if f2==1:
+        margin='NO'
+
+    cali='YES'
+    if f3==0:
+        cali='YES'
+    if f3==1:
+        cali='NO'
+
+    print("language : %s, margintext : %s, Calibrated : %s "%(lang,margin,cali))
 
 
 filelist=[]
@@ -14,6 +41,9 @@ maskimglist=[]
 for root, dirs, files in os.walk(imageLocation):
     for file in files:
         filelist.append(file)
+
+if 'img_feature.csv' in filelist:
+    filelist.remove('img_feature.csv')
 
 print(filelist)
 
@@ -33,20 +63,49 @@ print(len(maskimglist))
 if (len(originimglist)!=len(maskimglist)):
     print(" i think... something wrong here... ")
 
-f = open('img_feature.csv', 'w', encoding='utf-8', newline='')
+f = open(imageLocation+ '/' + 'img_feature.csv', 'w', encoding='utf-8', newline='')
 wr = csv.writer(f)
 
 data=[]
 for index in range(0,len(originimglist)):
+
     targetimg = cv2.imread(imageLocation + '/' + originimglist[index])
     cv2.imshow("Look at this image!", targetimg)
-    cv2.waitKey(0)
-    feature1 = input("Language? ENG(0),JPN(1) : ")
-    feature2 = input("is there margin-text? YES(1), NO(0) : ")
-    feature3 = input("is image calibrated? YES(1),NO(0)" )
+    print("What is language? JPN-(push q) / ENG- (push w)")
+    print("Is there margin text? YES-(push a) / NO - (push s)")
+    print("is calibrated? YES-(push z) / NO - (push x)")
+    print("----------------------------------------")
+    print("Is classifying finished?, push ESC")
+    feature1,feature2,feature3=0,0,0
+
+    while True:
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('q'):
+            feature1=0
+            stateprinter(feature1,feature2,feature3)
+        if k == ord('w'):
+            feature1=1
+            stateprinter(feature1, feature2, feature3)
+        if k == ord('a'):
+            feature2=0
+            stateprinter(feature1,feature2,feature3)
+        if k == ord('s'):
+            feature2=1
+            stateprinter(feature1, feature2, feature3)
+        if k == ord('z'):
+            feature3=0
+            stateprinter(feature1,feature2,feature3)
+        if k == ord('x'):
+            feature3=1
+            stateprinter(feature1, feature2, feature3)
+        if k == 27:
+            cv2.destroyAllWindows()
+            break
 
     data.append([originimglist[index],feature1,feature2,feature3])
 
 
-
-print(data)
+for i in range(0, len(data)):
+    print(data[i])
+    wr.writerow(data[i])
+f.close()
